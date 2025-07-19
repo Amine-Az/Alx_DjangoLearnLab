62b61ff4f5350163ababe742f3c8369f642f752c
Amine-Az <57254130+Amine-Az@users.noreply.github.com>
2025-07-19 14:39:49 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path('library/<int:pk>/detail/', views.library_detail_function, name='library_fbv_detail'),  # FBV
+    path('library/<int:pk>/view/', views.LibraryDetailView.as_view(), name='library_cbv_detail'),  # CBV
 ]
