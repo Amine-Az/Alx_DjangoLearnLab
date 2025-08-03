5a7031ca1a024d3b8f2869e04ea8f1b09474a640
Amine-Az <57254130+Amine-Az@users.noreply.github.com>
2025-08-03 16:00:31 +0100

    Add files via upload

---
+++ api_project/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Apply permission
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can perform CRUD
